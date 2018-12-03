a76e61cd3af75699a9ccaf08b75296aae16a572c
ruannvdw <ruannvdw@gmail.com>
2018-12-03 22:39:49 +0200

    development

--- my_blogsite/blog/views.py
+++ my_blogsite/blog/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 posts = [
     {
         'author': 'Corey',
-        'title': 'Blog_post01',
+        'title': 'Blog post01',
         'content': 'First post content',
         'date_posted': 'Dec 03, 2018'
     },
     {
         'author': 'rvdw',
-        'title': 'Blog_post02',
+        'title': 'Blog post02',
         'content': 'Second post content',
         'date_posted': 'Dec 01, 2017'
     }
@@ -19,10 +19,11 @@ posts = [
 
 def home_view(request):
     context = {
-        'posts': posts
+        'posts': posts,
+        'title': 'Blogs'
     }
     return render(request, 'blog/home.html', context)
 
 
 def about_view(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'About'})
