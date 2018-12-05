e5ede563652bc03f0e053d0c059746e02936d65f
ruannvdw <ruannvdw@gmail.com>
2018-12-05 19:59:35 +0200

    Merge branch 'master' of https://github.com/srvdws/blog_project_cs
    # Conflicts:
    #	my_blogsite/blog/migrations/0001_initial.py

--- my_blogsite/users/views.py
+++ my_blogsite/users/views.py
@@ -4,13 +4,14 @@ from django.contrib import messages
 
 
 def register_view(request):
-    print('accessed view page')
+    print('opened register page')
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         print('form is created')
         if form.is_valid():
             print('form is valid')
             username = form.cleaned_data.get('username')
+            print(username)
             messages.success(request, "Account Created for {}".format(username))
             return redirect('home_view')
     else:
