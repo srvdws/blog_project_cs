ed4bafdb7f6827b590f2ae766968f0e7aff4c0fe
srvdws <ruannvdw@gmail.com>
2018-12-05 07:40:57 +0200

    development

--- my_blogsite/blog/views.py
+++ my_blogsite/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Corey',
-        'title': 'Blog post01',
-        'content': 'First post content',
-        'date_posted': 'Dec 03, 2018'
-    },
-    {
-        'author': 'rvdw',
-        'title': 'Blog post02',
-        'content': 'Second post content',
-        'date_posted': 'Dec 01, 2017'
-    }
-]
+from . import models
 
 
 def home_view(request):
     context = {
-        'posts': posts,
+        'posts': models.PostModel.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
