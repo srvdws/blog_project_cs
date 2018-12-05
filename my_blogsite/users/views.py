b682cd31ac4767ae07e14edc63ebd3d191bf4831
srvdws <ruannvdw@gmail.com>
2018-12-05 10:18:26 +0200

    development

--- my_blogsite/users/views.py
+++ my_blogsite/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register_view(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account Created for {username}")
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'form': form})
