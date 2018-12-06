10c6b08f332e29b6a47f1572106c716dd57bf9d9
srvdws <ruannvdw@gmail.com>
2018-12-06 07:39:21 +0200

    development

--- my_blogsite/users/views.py
+++ my_blogsite/users/views.py
@@ -11,8 +11,8 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, "Account Created for {}".format(username))
-            return redirect('home_view')
+            messages.success(request, "Account Created for {}, Please log in.".format(username))
+            return redirect('login')
         else:
             messages.warning(request, "Please ensure all information is correct")
             form = forms.UserRegisterForm(request.POST)
