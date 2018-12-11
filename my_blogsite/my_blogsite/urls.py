8a60a8a297d09dcbac31ca63bf888c3b26089f07
srvdws <ruannvdw@gmail.com>
2018-12-11 16:04:52 +0200

    development

--- my_blogsite/my_blogsite/urls.py
+++ my_blogsite/my_blogsite/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from blog import urls as blog_urls
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(blog_urls)),
@@ -26,4 +29,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = "users/login.html"), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = "users/logout.html"), name = 'logout'),
     path('profile/', user_views.profile_view, name = "profile"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
