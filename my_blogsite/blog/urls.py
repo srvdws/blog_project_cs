006824ed2d40464497513ec1f4dc76543979be45
srvdws <ruannvdw@gmail.com>
2019-01-09 07:34:19 +0200

    development

--- my_blogsite/blog/urls.py
+++ my_blogsite/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_view, name = "home_view"),
+    path('', views.HomeListView.as_view(), name = "home_view"),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name = "post_detail"),
     path('about/', views.about_view, name = "about_view" )
 ]
--- my_blogsite/blog/views.py
+++ my_blogsite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -8,6 +9,17 @@ def home_view(request):
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
+
+
+class HomeListView(ListView):
+    model = models.PostModel
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = models.PostModel
 
 
 def about_view(request):
